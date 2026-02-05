1cd8eafa153f84d19a72782561a033618820c553
NIKHILxRAJ <raj.nikhilraj333@gmail.com>
2026-02-06 03:29:17 +0530

    Remove node_modules and add gitignore

---
+++ app.js
@@ -0,0 +1,47 @@
+
+  /**
+   *
+   * HMR - Hot Module Reloading 
+   * File Watcher algorithm - C++
+   * BUNDLING
+   * MINIFY
+   * Cleaning our Code
+   * Dev and Production Build
+   * Super Fast build algorithm
+   * Image Optimization
+   * Caching while development
+   * Compresion
+   *  Compatable with older version of browser 
+   * HTTPS on dev  
+   * port Number
+   *  Consistent Hashing Algorithm
+   * Zero Config
+   *  
+   */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// create elements with UNIQUE keys
+const heading1 = React.createElement(
+  "h1",
+  { id: "title", key: "heading-1" },
+  "Heading 1 from parcel" 
+);
+
+const heading2 = React.createElement(
+  "h2",
+  { id: "title2", key: "heading-2" },
+  "Heading 2 from parcel"
+);
+
+// parent container
+const container = React.createElement(
+  "div",
+  { id: "container" },
+  [heading1, heading2]
+);
+
+// create root and render
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
