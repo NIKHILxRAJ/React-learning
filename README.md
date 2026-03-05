19c1d8468195dab945effd3573159b255fc195da
NIKHILxRAJ <raj.nikhilraj333@gmail.com>
2026-03-06 01:23:54 +0530

    Today i learned about JSX

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 In this repo i am just learning react from Namaste React Course
 <br>
 author- Nikhil raj
+
+
+
+<!--  some important points regarding parcel
+
+{
+/**
+   * Important power of parcel...
+   * 
+   * HMR - Hot Module Reloading 
+   * File Watcher algorithm - C++
+   * BUNDLING
+   * MINIFY
+   * Cleaning our Code
+   * Dev and Production Build
+   * Super Fast build algorithm
+   * Image Optimization 
+   * Caching while development
+   * Compresion
+   *  Compatable with older version of browser 
+   * HTTPS on dev  
+   * port Number
+   *  Consistent Hashing Algorithm
+   * Zero Config
+   * Tree Shaking - Removing un-used code.
+   *  
+   */
+}
+ -->
--- app.js
+++ app.js
@@ -1,53 +1,25 @@
 
-  /**
-   *
-   * HMR - Hot Module Reloading 
-   * File Watcher algorithm - C++
-   * BUNDLING
-   * MINIFY
-   * Cleaning our Code
-   * Dev and Production Build
-   * Super Fast build algorithm
-   * Image Optimization
-   * Caching while development
-   * Compresion
-   *  Compatable with older version of browser 
-   * HTTPS on dev  
-   * port Number
-   *  Consistent Hashing Algorithm
-   * Zero Config
-   * Tree Shaking - Removing unused code.
-   *  
-   */
+  
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// create elements with UNIQUE keys
-const heading1 = React.createElement(
-  "h1",
-  { id: "title", key: "heading-1" },
-  "Heading 1 from parcel" 
-); 
-// React .createElement => Object => HTML (DOM)  // React Element  
+// React.createElement => Object => HTML(DOM)
 
-const heading2 = React.createElement(
-  "h2",
-  { id: "title2", key: "heading-2" },
-  "Heading 2 from parcel"
-);
-
-// parent container
-const container = React.createElement(
-  "div",
-  { id: "container" },
-  [heading1, heading2]
+const heading2 =
+( 
+  <h1 id="title" key="h2">
+   Namaste React
+  </h1>
 );
 
 
+//  React Component
 
-// adding some feature
 
 // create root and render
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing a react element inside the root 
+
+// asyn defer
 root.render(container);
